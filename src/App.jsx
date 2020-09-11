2e888f4d921dfaca9efc49a11049718f6a1d2eca
tienphong <tienphong111222@gmail.com>
2020-09-11 23:51:39 +0700

    set loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import 'antd/dist/antd.css';
 import { Header, ListItems } from './components/';
 import { loadProduct } from './store/reducer/Products';
 import Footer from './components/Footer';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Spin } from 'antd';
 const App = () => {
   const dispatch = useDispatch();
+  const {
+    ProductsReducer: { loading },
+  } = useSelector((state) => state);
+
   useEffect(() => {
     dispatch(loadProduct());
   }, [dispatch]);
   return (
     <React.Fragment>
       <Header />
+
       <div style={{ paddingTop: 200 }}>
-        <ListItems />
+        {loading ? (
+          <div className='flex j-center a-center' style={{ height: '70vh' }}>
+            <Spin size='large' />
+          </div>
+        ) : (
+          <ListItems />
+        )}
+
         <Footer />
       </div>
     </React.Fragment>
