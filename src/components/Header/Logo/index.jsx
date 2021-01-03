1731916f8e5f275556e8aab4e48a8f3f237a4f3d
Phong Nguyen <tienphong111222@gmail.com>
2021-01-03 09:52:26 +0700

    Fix logo

--- src/components/Header/Logo/index.jsx
+++ src/components/Header/Logo/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-
+import LogoPic from '../../../Logo.jpg';
 const Logo = () => {
   return (
     <div>
-      <img
-        src='https://1000logos.net/wp-content/uploads/2017/12/Pornhub-Logo.png'
-        alt=''
-        style={{ width: 300 }}
-      />
+      <img src={LogoPic} alt='' style={{ width: 300 }} />
     </div>
   );
 };
